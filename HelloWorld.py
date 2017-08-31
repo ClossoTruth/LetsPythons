661f1f3c81e6d0bf5fb55f8cbc16886f38b4abda
ClossoTruth <debuchyxzavier@gmail.com>
2017-08-31 23:56:53 +0800

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+
+//Hello World
+// Simple Print With HelloWorld
+
+print "Hello World"
+
+global coin
+coin = 0
+
+function HelloWorld()
+
+global items
+items = 1
+
+if(items == 1)
+ print "Hello"
+if(items == 0)
+ print "Nee"
+ 
+ 
+
